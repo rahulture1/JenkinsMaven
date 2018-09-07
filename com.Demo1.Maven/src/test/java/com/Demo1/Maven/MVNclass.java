9eb5bff1f421b717803f62c1140d114d8c136896
rahulture1 <rahul.a.ture@gmail.com>
2018-09-07 17:11:17 +0530

    Second Commit

--- com.Demo1.Maven/src/test/java/com/Demo1/Maven/MVNclass.java
+++ com.Demo1.Maven/src/test/java/com/Demo1/Maven/MVNclass.java
@@ -1,12 +1,28 @@
 package com.Demo1.Maven;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class MVNclass {
 	
 	@Test
-	public void simple() {
-		System.out.println("Welcome to Jenkins");
+	public void test() throws InterruptedException {
+	System.setProperty("webdriver.chrome.driver","D:\\software\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver ();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+	driver.get("https://www.wikipedia.org/");
+	System.out.println(driver.getTitle());
+    Select select = new Select(driver.findElement(By.xpath(".//*[@id='searchLanguage']")));
+    select.selectByValue("hi");
+    select.selectByIndex(19);
+    Thread.sleep(3000);
+    driver.close();   
 		
 	}
 
